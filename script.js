ffdee11771432ee82c95ec6eefb0bc954e290028
erickariasec <erickarias1999@hotmail.com>
2022-05-07 16:09:34 -0500

    Portfolio, erickariasec.github.io, adding own text in about me description

--- script.js
+++ script.js
@@ -59,4 +59,17 @@ $(document).ready(function(){
             }
         }
     });
+
+    // send email
+    // function sendEmail(){
+    //     Email.send({
+    //         Host: "smtp.yourisp.com",
+    //         Username: "username",
+    //         Password: "password",
+    //         To: "them@website.com",
+    //         From: "you@isp.com",
+    //         Subject: "This is the subject",
+    //         Body: "And this is the body",
+    //     }).then((message) => alert("mail sent successfully"));
+    // }
 });
