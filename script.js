633347c6f3c418aadd12edba44994a507008d715
erickariasec <erickarias1999@hotmail.com>
2022-05-07 11:47:59 -0500

    Portfolio, erickariasec.github.io, typing animation fixed

--- script.js
+++ script.js
@@ -25,17 +25,17 @@ $(document).ready(function(){
 
     // typing animation script
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Designer", "Freelancer"],
+        strings: ["Developer", "Freelancer", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
-        loop: true
+        loop: true,
     });
 
     var typed = new Typed(".typing", {
-        strings: ["Developer", "Designer", "Freelancer"],
+        strings: ["Developer", "Freelancer", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
-        loop: true
+        loop: true,
     });
 
     // owl carousel script
